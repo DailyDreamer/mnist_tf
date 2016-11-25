a84df72efee9594f6fe4d4cb2b0fa507b5b495f4
李心成 <lixc12@lixinchengdeiMac.local>
2016-11-25 17:27:43 +0800

    init

---
+++ mlp.py
@@ -0,0 +1,32 @@
+from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
+import tensorflow as tf
+
+def main(_):
+  mnist = read_data_sets('./mnist', one_hot=True)
+
+  # placeholder for input, None for any batch size
+  x = tf.placeholder(tf.float32, [None, 784])
+  W = tf.Variable(tf.zeros([784, 10]))
+  b = tf.Variable(tf.zeros([10]))
+  y = tf.matmul(x, W) + b
+  # y_ for label input
+  y_ = tf.placeholder(tf.float32, [None, 10])
+
+  cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
+  train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+  sess = tf.InteractiveSession()
+  # Train
+  tf.initialize_all_variables().run()
+  for _ in range(1000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+  # Test trained model
+  correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+  print(sess.run(accuracy, feed_dict={x: mnist.test.images,
+                                      y_: mnist.test.labels}))
+
+if __name__ == '__main__':
+  tf.app.run()
