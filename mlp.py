8bfb313a0de5f710485614d096b6455cc94ba48a
李心成 <lixc12@lixinchengdeiMac.local>
2016-11-28 17:37:01 +0800

    make it more clear

--- mlp.py
+++ mlp.py
@@ -2,7 +2,7 @@ from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
 import tensorflow as tf
 
 def main(_):
-  mnist = read_data_sets('./mnist', one_hot=True)
+  mnist_data = read_data_sets('./mnist', one_hot=True)
 
   # placeholder for input, None for any batch size
   x = tf.placeholder(tf.float32, [None, 784])
@@ -19,14 +19,14 @@ def main(_):
   # Train
   tf.initialize_all_variables().run()
   for _ in range(1000):
-    batch_xs, batch_ys = mnist.train.next_batch(100)
+    batch_xs, batch_ys = mnist_data.train.next_batch(100)
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
   # Test trained model
   correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
   accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-  print(sess.run(accuracy, feed_dict={x: mnist.test.images,
-                                      y_: mnist.test.labels}))
+  print(sess.run(accuracy, feed_dict={x: mnist_data.test.images,
+                                      y_: mnist_data.test.labels}))
 
 if __name__ == '__main__':
   tf.app.run()
--- mnist.py
+++ mnist.py
@@ -1,43 +1,7 @@
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-"""Builds the MNIST network.
-
-Implements the inference/loss/training pattern for model building.
-
-1. inference() - Builds the model as far as is required for running the network
-forward to make predictions.
-2. loss() - Adds to the inference model the layers required to generate loss.
-3. training() - Adds to the loss model the Ops required to generate and
-apply gradients.
-
-This file is used by the various "fully_connected_*.py" files and not meant to
-be run.
-"""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import math
-
 import tensorflow as tf
 
-# The MNIST dataset has 10 classes, representing the digits 0 through 9.
 NUM_CLASSES = 10
-
-# The MNIST images are always 28x28 pixels.
 IMAGE_SIZE = 28
 IMAGE_PIXELS = IMAGE_SIZE * IMAGE_SIZE
 
